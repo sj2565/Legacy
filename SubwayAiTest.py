dd32cee9492de09bf3034db02c42140ed2eca999
sj2565 <seojoon2565@gmail.com>
2025-02-21 03:32:33 +0900

    subway test

--- SubwayAiTest.py
+++ SubwayAiTest.py
@@ -2,13 +2,22 @@ import sys
 import json
 import numpy as np
 from sklearn.ensemble import IsolationForest
+import os
+import pandas as pd
 
 # 센서 데이터 불러오기
 file_path = "/home/seojoon/nodetest/sensor_data.csv"
 
+#if os.path.exists(file_path):
+#	print("ok")
+#else :
+#	print("X")	
+
 try:
-	df = pd.read_csv(file_path, names=["distance", "temperature"])
+	df = pd.read_csv(file_path, names=["distance", "temperature"] ,header=None)
 	training_data = df.to_numpy() # 배열로 변환
+	print("CSV 파일이 정상적으로 로드됨!" ,file = sys.stderr)
+	print(df.head(), file = sys.stderr)
 
 except:
 	print("센서 데이터 파일이 없습니다! 기본값 사용")
@@ -25,11 +34,23 @@ model = IsolationForest(
 )
 model.fit(training_data)
 
+print("AI 모델 실행됨", file=sys.stderr)
+
 # Node.js에서 실시간 데이터 입력 받기 
 for line in sys.stdin:
     try:
+        print(f" 받은 원본  데이터 : {repr(line.strip())}", file=sys.stderr)
+        print(f" 받은 데이터 타입 : {type(line.strip())}", file=sys.stderr)
         # JSON 데이터 읽기 
         data = json.loads(line.strip())
+        print(f" jSON 파싱된 데이터 : {data}", file=sys.stderr)  # JSON 정상 파싱 확인
+        
+        if not isinstance(data, dict):
+            raise ValueError(" 데이터가 JSON 객체가 아님")
+	
+        if "distance" not in data or "temperature" not in data:
+            raise ValueError("키가 존재하지 않음")
+
         new_data = np.array([[data["distance"], data["temperature"]]])
 
         # 모델
--- SubwayServer.js
+++ SubwayServer.js
@@ -2,13 +2,31 @@ const WebSocket = require('ws');
 const {spawn} = require('child_process');
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+
+
+
+
+
+//const aiScriptPath = path.resolve(__dirname, "SubwayAiTest.py");
+//console.log(`실행할 AI모델 경로 : ${aiScriptPath}`);
+
+
+
 const sensorProcess = spawn("./SubwaySensor"); // C언어 
-const aiProcess = spawn("./SubwayAiTest.py"); // AI 모델 실행
+const aiProcess = spawn("python3" ,["SubwayAiTest.py"], {encoding : "utf8"}); // AI 모델 실행
+
+//const sensorData = JSON.stringify({distance : 15, tmperature : 30});
+//console.log("AI 모델에 보낼 데이터 : " ,sensorData);
+//console.log("데이터 타입 : ", typeof sensorData);
+//aiProcess.stdin.write(sensorData + "\n");
+aiProcess.stdin.end();
+
 
 sensorProcess.stdout.on("data", (data) => {
 	const messages = data.toString().trim().split("\n"); // 여러 개의 메세지가 한번에 들어올 경우 처리
@@ -33,12 +51,14 @@ sensorProcess.stdout.on("data", (data) => {
 });
 
 aiProcess.stdout.on("data", (data) => {
+	console.error("AI 모델 오류 로그: " ,data.toString().trim()); 
 	const messages = data.toString().trim().split("\n");
-	message.forEach((message) => {
-		console.log("AI 분석 결과 : " ,message);
+	messages.forEach((message) => {
+		console.log("AI 분석 결과 (처리 전) : " ,message);
 		
 		try {
 			const result = JSON.parse(message);
+			console.log("AI 분석 결과 (처리 후) : " ,result);
 			
 			// 웹 클라이언트로 전송
 			wss.clients.forEach(client => {
@@ -48,6 +68,7 @@ aiProcess.stdout.on("data", (data) => {
 			});
 		} catch (error) {
 			console.error("AI 결과 JSON 파싱 오류 발생 : " ,error);
+			console.error("AI 프로세스 출력 원본 데이터 : " ,message);
 		}
 	});
 });
