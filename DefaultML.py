2fcdf3f3fe426a616d28da068ad3426825a68dc8
sj2565 <seojoon2565@gmail.com>
2025-03-02 17:32:18 +0900

    final test

--- DefaultML.py
+++ DefaultML.py
@@ -8,13 +8,13 @@ from sklearn.ensemble import IsolationForest
 print("AI 모델 실행됨! (CSV 기반 초기 학습 + 실시간 분석 시작)", file=sys.stderr)
 
 file_path = "sensor_data.csv"
-df = pd.read_csv(file_path, names=["distance", "temperature"], header=None)
+df = pd.read_csv(file_path, names=["distance", "temperature", "pressure"], header=None)
 training_data = df.to_numpy()
 
 model = IsolationForest(n_estimators=200,	
         contamination=0.05,					
         max_samples=256,					
-        max_features=2,						
+        max_features=3,						
         random_state=42)					
 model.fit(training_data)
 '''
@@ -38,15 +38,17 @@ while True:
         data = json.loads(line)
         distance = data["distance"]
         temperature = data["temperature"]
+        pressure = data["pressure"]
 
-        new_data = np.array([[distance, temperature]])
+        new_data = np.array([[distance, temperature, pressure]])
         prediction = model.predict(new_data)
         result = "normal" if prediction[0] == 1 else "anomaly"
 
         output = json.dumps({
             "status": result,
             "distance": distance,
-            "temperature": temperature
+            "temperature": temperature,
+            "pressure": pressure
         })
         print(output)
         sys.stdout.flush()
--- SubwayML.py
+++ SubwayML.py
@@ -12,17 +12,20 @@ print("AI 모델 실행됨! (실시간 이상 탐지 학습 시작, 튜닝 O)", 
 
 # CSV 파일 로드
 file_path = "sensor_data.csv"
-df_raw = pd.read_csv(file_path, names=["distance", "temperature"], header=None, dtype={"distance": np.float32, "temperature": np.float32})
-df = pd.read_csv(file_path, names=["distance", "temperature"], header=None, dtype={"distance": np.float32, "temperature": np.float32})
+df_raw = pd.read_csv(file_path, names=["distance", "temperature", "pressure"], 
+header=None, dtype={"distance": np.float32, "temperature": np.float32, "pressure": np.float32})
+
+df = pd.read_csv(file_path, names=["distance", "temperature", "pressure"], 
+header=None, dtype={"distance": np.float32, "temperature": np.float32, "pressure": np.float32})
 
 '''
-# 원본 데이터로 진행한 모델 학습
+# 원본 데이터로 진행한 기본 모델 학습
 training_data_raw = df_raw.to_numpy()
 
 model_raw = IsolationForest(n_estimators=100,	# 트리 개수 증가
         contamination=0.05,					# 이상값 비율 낮춤 (더 민감)
         max_samples=256,					# 트리 샘플 크기 설정
-        max_features=2,						# 특징 수(거리, 온도)
+        max_features=3,						# 특징 수(거리, 온도)
         random_state=42)					# 재현 가능성 유지
 model_raw.fit(training_data_raw)
 
@@ -37,7 +40,7 @@ print("\n 원본 데이터 기반 이상값 탐지 결과 :")
 print(original_anomalies)
 '''
 
-# 데이터 전처리 과정 후 모델 학습
+# 데이터 전처리 과정 후 튜닝 모델 학습
 # 결측값 처리 (NaN값 보정)
 df.interpolate(method="linear", inplace=True) # 선형 보간(데이터 흐름 유지)
 df.fillna(df.mean(), inplace=True) # 평균값으로 NaN값 보정
@@ -48,27 +51,29 @@ df.fillna(df.mean(), inplace=True) # 평균값으로 NaN값 보정
 
 # 데이터 정규화
 scaler = MinMaxScaler()
-scaler.fit(df[["distance", "temperature"]]) # 최소값과 최대값을 찾아서 transform에 적용 => 실시간 데이터는 CSV 데이터의 최소/최대 값을 기준으로 변환
-df[["distance", "temperature"]] = scaler.transform(df[["distance", "temperature"]])
+scaler.fit(df[["distance", "temperature", "pressure"]]) # 최소값과 최대값을 찾아서 transform에 적용 => 실시간 데이터는 CSV 데이터의 최소/최대 값을 기준으로 변환
+df[["distance", "temperature", "pressure"]] = scaler.transform(df[["distance", "temperature", "pressure"]])
 
 # 이동 평균(SMA) 추가로 인한 노이즈 완화)
 #df["sma_distance"] = df["distance"].rolling(window=3).mean()
 #df["sma_temp"] = df["temperature"].rolling(window=3).mean()
+#df["sma_pressure"] =df["pressure"].rolling(window=3).mean()
 
 # Sliding Window 적용 (과거 데이터 반영)
 df["prev_distance_1"] = df["distance"].shift(1)
 df["prev_temp_1"] = df["temperature"].shift(1)
+df["prev_pressure_1"] = df["pressure"].shift(1)
 df.dropna(inplace=True)  # NaN값 제거
 
 # 최종 데이터 
-training_data = df.to_numpy() # (n_sample, 4) => distance, temperature, prev_dis, prev_temp
+training_data = df.to_numpy() # (n_sample, 6) => distance, temperature, pressure, prev_dis, prev_temp, prev_pressure
 #print(training_data)
 
 # AI 모델 학습
 model = IsolationForest(n_estimators=200,	# 트리 개수 증가
-        contamination=0.01,					# 이상값 비율 낮춤 (더 민감)
-        max_samples="auto",					# 트리 샘플 크기 설정
-        max_features=4,						# 특징 수(거리, 온도, 이전 거리, 이전 온도)
+        contamination=0.01,					# 이상값 비율 낮춤(더 민감)
+        max_samples="auto",					# 트리 샘플 크기 설정(자동)
+        max_features=6,						# 특징 수(거리, 온도, 압력, 이전 거리, 이전 온도, 이전 압력)
         random_state=42)					# 재현 가능성 유지
 model.fit(training_data)
 
@@ -95,43 +100,49 @@ while True:
         data = json.loads(line)
         distance = float(data.get("distance", np.nan)) # 겉측값 방지
         temperature = float(data.get("temperature", np.nan))
+        pressure = float(data.get("pressure", np.nan))
        
         # 결측값 처리 (NaN -> 이전 값으로 대체)
         if np.isnan(distance):
             distance = prev_distance if prev_distance is not None else df["distance"].mean()
         if np.isnan(temperature):
             temperature = prev_temperature if prev_temperature is not None else df["temperature"].mean()
+        if np.isnan(pressure):
+			pressure = prev_pressure if prev_pressure is None else df["pressure"].mean()    
             	
         # 이상값 제거 (Z-Score, (데이터 - 평균) / 표준편차)
         std_distance = df["distance"].std()
         std_temperature = df["temperature"].std()
+        std_pressure = df["pressure"].std()
                 
         z_scores_distance = (distance - df["distance"].mean()) / std_distance
         z_scores_temperature = (temperature - df["temperature"].mean()) / std_temperature
+        z_scores_pressure = (pressure - df["pressure"].mean()) / std_pressure
        
-        if abs(z_scores_distance) > 4.0 or abs(z_scores_temperature) > 4.0:
+        if abs(z_scores_distance) > 4.0 or abs(z_scores_temperature) > 4.0 or abs(z_scores_pressure) > 4.0:
             anomaly_flag = True
         else:
             anomaly_flag = False
 		
         # 정규화 작업 실시, 위에 데이터 전처리 과정이랑 일관성 갖기, scaler.fit() 재호출 X              
-        new_data_2d = np.array([[distance, temperature]])
-        new_data_2d = scaler.transform(new_data_2d)
-		
+        new_data_3d = np.array([[distance, temperature, pressure]])
+        new_data_3d = scaler.transform(new_data_3d)
+        
         #  Sliding Window 적용, 학습 시와 실시간 시 동일 차원으로 맞추기 
         if "prev_distance" not in locals():
-            prev_distance, prev_temperature = distance, temperature # 초기값 설정
-        new_data_4d= np.array([[new_data_2d[0, 0], new_data_2d[0, 1], prev_distance, prev_temperature]])
-        prev_distance, prev_temperature = new_data_2d[0, 0], new_data_2d[0, 1] # 이전 값 업데이트
+            prev_distance, prev_temperature, prev_pressure = distance, temperature, pressure # 초기값 설정
+        new_data_6d= np.array([[new_data_3d[0, 0], new_data_3d[0, 1], new_data_3d[0, 2], prev_distance, prev_temperature, prev_pressure]])
+        prev_distance, prev_temperature, prev_pressure = new_data_3d[0, 0], new_data_3d[0, 1], new_data_3d[0, 2] # 이전 값 업데이트
 
         # 모델 예측 수행
-        prediction = model.predict(new_data_4d)
+        prediction = model.predict(new_data_6d)
         result = "normal" if prediction[0] == 1 else "anomaly"
 		
         output = json.dumps({
             "status": result,
             "distance": float(distance),
-            "temperature": float(temperature)
+            "temperature": float(temperature),
+            "pressure": float(pressure)
         })
         print(output)
         sys.stdout.flush()
